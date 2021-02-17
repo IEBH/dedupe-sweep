39cd820f7325fa0c5cf05a5d6e4f1ad5b9eb3662
Connor Forbes <connorforbes25@gmail.com>
2021-02-18 09:12:57 +1000

    chore: Remove it.only

--- test/mutators.js
+++ test/mutators.js
@@ -50,7 +50,7 @@ describe('Mutators', ()=> {
 		expect(dedupe.mutators.removeEnclosingBrackets.handler('(One)')).to.equal('One');
 	});
 
-	it.only('consistentPageNumbering', ()=> {
+	it('consistentPageNumbering', ()=> {
 		expect(dedupe.mutators.consistentPageNumbering.handler('244-58')).to.equal('244-258');
 		expect(dedupe.mutators.consistentPageNumbering.handler('244-258')).to.equal('244-258');
 		expect(dedupe.mutators.consistentPageNumbering.handler('244-8')).to.equal('244-248');
